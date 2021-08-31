44a3466daab76e75e9b3c94ba89b112aa7375ffe
ukeme <ukeme.wilson@gmail.com>
2021-08-31 15:32:54 +0100

    extra changes

--- get_weekly_tweet_count.py
+++ get_weekly_tweet_count.py
@@ -1,4 +1,3 @@
-from traceback import print_tb
 import twint
 import pandas as pd
 from datetime import datetime, timedelta
@@ -10,6 +9,7 @@ pd.options.mode.chained_assignment = None
 
 
 MONGO_URL = env_config("MONGO_URL")
+
 client = MongoClient(MONGO_URL)
 
 db = client.twitter_users
@@ -17,8 +17,8 @@ collection = db.weekly_count
 
 
 today = datetime.now()
-start_date = today + timedelta(-11)
-end_date = today + timedelta(-5)
+start_date = today + timedelta(-7)
+end_date = today + timedelta(-1)
 
 
 start_date_str = start_date.strftime("%Y-%m-%d")
@@ -47,7 +47,11 @@ def get_latest_tweets_from_handle(username, num_tweets,start_date,  end_date):
         print(e)
         tweet_df = pd.DataFrame()
 
-    return len(tweet_df)
+    tweet_count = len(tweet_df)
+
+    ave_replies = round(sum(tweet_df['nreplies'].tolist()) / tweet_count)
+
+    return tweet_count, ave_replies 
 
 
 def get_record_details(search_dict, collection, find_one=True):
@@ -67,17 +71,24 @@ def insert_records(collection, record):
 
 def save_to_mongo_db(data):
     insert_records(collection, data)
+    cur = collection.count_documents({})
+    print(f"we have {cur} entries")
 
 def get_weekly_count(username):
 
     try:
-        num_of_tweets = get_latest_tweets_from_handle(username, 1000, start_date_str, end_date_str)
-    except:
-        num_of_tweets = 'NA'
+        tweet_count, ave_replies = get_latest_tweets_from_handle(username, 2500, start_date_str, end_date_str)
+    except Exception as e:
+        tweet_count = 0
+        ave_replies = 0
+        print(e)
 
     data = {
         'username' : username,
-        'num_of_tweets' :  num_of_tweets
+        'num_of_tweets' :  tweet_count,
+        'ave_replies': ave_replies,
+        'start_date': start_date_str,
+        'end_date': end_date_str
     }    
 
     save_to_mongo_db(data)
@@ -88,7 +99,7 @@ def run_script():
         twitter_users_df = pd.read_csv("twitter_details_collections.csv", dtype="unicode")
         usernames = twitter_users_df['handle'].tolist()
 
-        for username in usernames:
+        for username in usernames[:3]:
             print(username)
             search_dict = {'username': username}
             query = get_record_details(search_dict, collection, find_one=True)
